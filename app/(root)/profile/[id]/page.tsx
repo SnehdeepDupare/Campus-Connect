f0331b1fbc8fe54a429b535b46703a60d4770eac
Snehdeep <duparesnehdeep@gmail.com>
2024-08-14 00:45:52 +0530

    remove shadcn tabs

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -2,13 +2,9 @@ import Image from "next/image";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
 import { fetchUser } from "@/lib/actions/user";
-import { profileTabs } from "@/constants";
 import { ProfileHeader } from "@/components/shared/profile-header";
 import { PostsTab } from "@/components/shared/posts-tab";
-import { LucideIcon } from "lucide-react";
 
 const ProfilePage = async ({ params }: { params: { id: string } }) => {
   const user = await currentUser();
@@ -27,43 +23,18 @@ const ProfilePage = async ({ params }: { params: { id: string } }) => {
         bio={userInfo.bio}
       />
 
+      <div className="mt-12 border-b">
+        <p className="border-b-2 w-fit border-b-primary font-bold text-lg ">
+          Posts
+        </p>
+      </div>
+
       <div className="mt-9">
-        <Tabs defaultValue="posts" className="w-full">
-          <TabsList className="flex min-h-[50px] flex-1 items-center gap-3 dark:bg-[#121417] text-light-2 data-[state=active]:bg-[#0e0e12] data-[state=active]:text-[#efefef]">
-            {profileTabs.map((tab) => (
-              <TabsTrigger key={tab.label} value={tab.value} className="tab">
-                <Image
-                  src={tab.icon}
-                  alt={tab.label}
-                  width={24}
-                  height={24}
-                  className="object-contain"
-                />
-
-                <p className="max-sm:hidden">{tab.label}</p>
-
-                {tab.label === "Posts" && (
-                  <p className="ml-1 rounded-sm bg-[#5C5C7B] px-2 py-1 text-xs font-medium text-[#efefef]">
-                    {userInfo.posts.length}
-                  </p>
-                )}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-          {profileTabs.map((tab) => (
-            <TabsContent
-              key={`content-${tab.label}`}
-              value={tab.value}
-              className="w-full"
-            >
-              <PostsTab
-                currentUserId={user.id}
-                accountId={userInfo.id}
-                accountType="User"
-              />
-            </TabsContent>
-          ))}
-        </Tabs>
+        <PostsTab
+          currentUserId={user.id}
+          accountId={userInfo.id}
+          accountType="User"
+        />
       </div>
     </section>
   );
